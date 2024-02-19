06dc860e7c74ea70e9ecd6b51ebd45caa1c671e0
Natalia <natalia.valova@web.de>
2024-02-19 14:36:43 +0100

    refactoring add useStates and Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
+import {Counter} from "./counter/Counter";
+import {Settings} from "./settings/Settings";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [min, setMin] = useState<number>(0)
+    const [max, setMax] = useState<number>(0)
+    const initialCount = min ? min : parseInt(localStorage.getItem('count')!);
+    const [count, setCount] = useState<number>(initialCount)
+
+    return (
+        <div className='app-wrapper'>
+
+
+            <Routes>
+                <Route path={"/*"} element={<Settings count={min}
+                                                     setCount={setCount}
+                                                     setMax={setMax}
+                                                     setMin={setMin}
+                                                     max={max}
+                                                     min={min}
+                />}/>
+                <Route path={"counter"} element={<Counter min={min}
+                                                          max={max}
+                                                          count={count}
+                                                          setCount={setCount}
+                />}/>
+
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
+
